5da7807c715524ce66ad186b69ee74d05716fb66
Timo Reitz <timo.reitz@4com.de>
2020-12-18 12:28:36 +0100

    Minor typo fix.

--- src/Company.php
+++ src/Company.php
@@ -22,6 +22,6 @@ class Company
     $applicantName = $application->getApplicantsName();
     $position = $application->getPositionName();
     echo "Hey, gut, Du hast es geschafft! Schön, dass Du mitgemacht hast und Dich für 4Com interessierst. Schicke uns doch einfach Deine Bewerbung, dann können wir uns kennenlernen und vielleicht schon bald gemeinsam Neues denken und Neues schaffen!" . PHP_EOL . PHP_EOL;
-    echo "Alle weiteren Informationen findest du unter: https://www.4com.de/karriere/software-developer-fullstack-php/" . PHP_EOL;
+    echo "Alle weiteren Informationen findest Du unter: https://www.4com.de/karriere/software-developer-fullstack-php/" . PHP_EOL;
   }
 }
